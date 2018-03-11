0b824b875b4e6be46854ce0a8d39472b1b5a5e0f
ie699416 <ie699416@iteso.mx>
2018-03-10 19:55:12 -0600

    Tarea Funcional

--- main.c
+++ main.c
@@ -13,6 +13,9 @@
 #include "FreeRTOS.h"
 #include "task.h"
 #include "event_groups.h"
+#include "queue.h"
+#include "semphr.h"
+
 
 #define EVENT_60_SECONDS (1<<0)
 #define EVENT_60_MINUTES (1<<1)
@@ -25,44 +28,64 @@
 #define ALARM_VALUE_MINUTES 0
 #define ALARM_VALUE_HOURS 1
 
+#define QUEUE_LENGTH 3
+#define QUEUE_ITEM_SIZE sizeof(time_msg_t)
+
 #define ALARM_MASK 0x01C
 
+SemaphoreHandle_t mutex;
+
+
+typedef enum {
+	seconds_type, minutes_type, hours_type
+} time_types_t;
+
+typedef struct {
+	time_types_t time_type;
+	uint8_t value;
+} time_msg_t;
+
+QueueHandle_t g_time_mailbox;
 EventGroupHandle_t g_time_events;
 
 void seconds_task(void *arg) {
 	TickType_t xLastWakeTime;
+	time_msg_t xMessage = { seconds_type, 55 };
 
 	const TickType_t xPeriod = pdMS_TO_TICKS(1000);
 	xLastWakeTime = xTaskGetTickCount();
 
-	uint8_t seconds = 55;
-
 	for (;;) {
+		xQueueSendToBack(g_time_mailbox, &xMessage, 10);
 
-		if (ALARM_VALUE_SECONDS == seconds) {
+		if (ALARM_VALUE_SECONDS == xMessage.value) {
 			xEventGroupSetBits(g_time_events, EVENT_ALARM_SECONDS);
 		} else {
 			xEventGroupClearBits(g_time_events, EVENT_ALARM_SECONDS);
 		}
 
 		vTaskDelayUntil(&xLastWakeTime, xPeriod);
-		seconds++;
+		xMessage.value++;
 
-
-		if (60 == seconds)
+		if (60 == xMessage.value)
 
 		{
-			seconds = 0;
+			xMessage.value = 0;
 			xEventGroupSetBits(g_time_events, EVENT_60_SECONDS);
 		}
 
 	}
 }
 void minutes_task(void *arg) {
-	uint8_t minutes = 59;
+	time_msg_t xMessage = { minutes_type, 59 };
 	for (;;) {
 
-		if (ALARM_VALUE_MINUTES == minutes) {
+
+		xQueueSendToBack(g_time_mailbox, &xMessage, 10);
+
+
+
+		if (ALARM_VALUE_MINUTES == xMessage.value) {
 			xEventGroupSetBits(g_time_events, EVENT_ALARM_MINUTES);
 		} else {
 			xEventGroupClearBits(g_time_events, EVENT_ALARM_MINUTES);
@@ -70,21 +93,27 @@ void minutes_task(void *arg) {
 
 		xEventGroupWaitBits(g_time_events, EVENT_60_SECONDS, pdTRUE, pdTRUE,
 		portMAX_DELAY);
-		minutes++;
+		xMessage.value++;
 
-
-		if (60 == minutes) {
-			minutes = 0;
+		if (60 == xMessage.value) {
+			xMessage.value = 0;
 			xEventGroupSetBits(g_time_events, EVENT_60_MINUTES);
 
 		}
 	}
 }
 void hours_task(void *arg) {
-	uint8_t hours = 0;
+	time_msg_t xMessage = { hours_type, 0 };
 	for (;;) {
 
-		if (ALARM_VALUE_HOURS == hours) {
+
+
+		xQueueSendToBack(g_time_mailbox, &xMessage, 10);
+
+
+
+
+		if (ALARM_VALUE_HOURS == xMessage.value) {
 			xEventGroupSetBits(g_time_events, EVENT_ALARM_HOURS);
 		} else {
 			xEventGroupClearBits(g_time_events, EVENT_ALARM_HOURS);
@@ -92,29 +121,65 @@ void hours_task(void *arg) {
 
 		xEventGroupWaitBits(g_time_events, EVENT_60_MINUTES, pdTRUE, pdTRUE,
 		portMAX_DELAY);
-		hours++;
+		xMessage.value++;
 
-
-		if (24 == hours) {
-			hours = 0;
+		if (24 == xMessage.value) {
+			xMessage.value = 0;
 		}
 	}
 }
 
 void print_task(void *arg) {
+
+	time_msg_t xMessage = { 0, 0 };
+	time_msg_t TimeReceived[3] = { { seconds_type, 0 }, { minutes_type, 0 }, {
+			hours_type, 0 } };
+
 	while (1) {
-		vTaskDelay(1000);
+		if ( xQueueReceive( g_time_mailbox, &xMessage, portMAX_DELAY ) != pdPASS) {
+			/* Nothing was received from the queue – even after blocking to wait
+			 for data to arrive. */
+		} else {
+			switch (xMessage.time_type) {
+			case seconds_type: {
+ 				TimeReceived[0].value = xMessage.value;
+ 				PRINTF("\n%d:%d:%d", TimeReceived[2].value, TimeReceived[1].value,
+ 											TimeReceived[0].value);
+ 				break;
+
+			}
+			case minutes_type: {
+				TimeReceived[1].value = xMessage.value;
+				break;
+
+			}
+			case hours_type: {
+				TimeReceived[2].value = xMessage.value;
+				break;
+
+			}
+
+			}
+
+
+		}
+
+
 	}
 }
 
 void alarm_task(void *arg) {
 	EventBits_t alarm;
 	while (1) {
-		alarm = xEventGroupGetBits(  g_time_events );
-		if (ALARM_MASK == (ALARM_MASK & alarm)){
-			PRINTF("ALARM");
-		}
+		alarm = xEventGroupGetBits(g_time_events);
+		if (ALARM_MASK == (ALARM_MASK & alarm)) {
 
+			xSemaphoreTake(mutex,portMAX_DELAY);
+			PRINTF("\nALARM");
+			xEventGroupClearBits(g_time_events, ALARM_MASK);
+			xSemaphoreGive(mutex);
+
+		}
 
 	}
 }
@@ -127,14 +192,18 @@ int main(void) {
 	/* Init FSL debug console. */
 	BOARD_InitDebugConsole();
 
-	g_time_events = xEventGroupCreate();
+	 mutex = xSemaphoreCreateMutex();
 
-	xTaskCreate(seconds_task, "Seconds", 300, NULL, configMAX_PRIORITIES - 1,
+	g_time_events = xEventGroupCreate();
+	g_time_mailbox = xQueueCreate(QUEUE_LENGTH, QUEUE_ITEM_SIZE);
+
+	xTaskCreate(seconds_task, "Seconds", 300, NULL, configMAX_PRIORITIES -2,
 	NULL);
-	xTaskCreate(minutes_task, "Minutes", 300, NULL, configMAX_PRIORITIES - 1,
+	xTaskCreate(minutes_task, "Minutes", 300, NULL, configMAX_PRIORITIES-1,
 	NULL);
-	xTaskCreate(hours_task, "Hours", 300, NULL, configMAX_PRIORITIES - 1, NULL);
-	xTaskCreate(print_task, "Printer", 300, NULL, configMAX_PRIORITIES - 1,
+	xTaskCreate(hours_task, "Hours", 300, NULL, configMAX_PRIORITIES  ,
+	NULL);
+	xTaskCreate(print_task, "Printer", 300, NULL, configMAX_PRIORITIES - 4,
 	NULL);
 	xTaskCreate(alarm_task, "Alarm", 300, NULL, configMAX_PRIORITIES - 3,
 	NULL);
